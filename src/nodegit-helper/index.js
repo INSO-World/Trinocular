b97bf5397dc48bfb19f1d44ac60d3f438f23eac3
Matthias Preymann <matthias.preymann@student.tuwien.ac.at>
2024-11-03 21:24:22 +0100

    Repo: Setup nodegit inside docker

---
+++ src/nodegit-helper/index.js
@@ -0,0 +1,4 @@
+
+import nodegit from 'nodegit';
+
+export default nodegit;
---
+++ src/repo/index.js
@@ -0,0 +1,6 @@
+
+import nodegit from '../nodegit-helper/index.js';
+
+console.log( 'clone:', nodegit.Clone );
+
+setInterval(() => console.log('servas'), 20* 1000);
