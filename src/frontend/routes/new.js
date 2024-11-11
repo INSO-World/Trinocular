44fc132488f7acfa1e5f2722f128b64f638f98c5
Matthias Preymann <matthias.preymann@student.tuwien.ac.at>
2024-11-11 12:02:43 +0100

    Frontend: Show error messages on form pages
    Added a basic system for displaying error messages in the UI, when form validation fails.
    
    #16

--- src/frontend/routes/new.js
+++ src/frontend/routes/new.js
@@ -1,26 +1,49 @@
-import { createToken } from '../lib/csrf.js';
 
-export function getNewRepoPage(req, res) {
+import Joi from 'joi';
+import { createToken } from '../lib/csrf.js';
+import { ErrorMessages } from '../lib/error-messages.js';
+
+const newRepositoryValidator= Joi.object({
+  name: Joi.string().trim().min(0).required().label('Name'), // The name may be empty, so we try to load it via the API
+  url: Joi.string().uri().required().label('URL'),
+  authToken: Joi.string().trim().required().label('Authentication Token'),
+  type: Joi.string().valid('github', 'gitlab').required().label('Type')
+}).unknown(true).required(); // Allow unknown fields for other stuff like csrf tokens
+
+function renderNewRepoPage(req, res, name, url, authToken, errorMessage) {
+  // FIXME: This (and the hbs-template) does not have support for different repo types
   res.render('new', {
     user: req.user,
-    csrfToken: createToken( req.sessionID )
+    csrfToken: createToken( req.sessionID ),
+    errorMessage,
+    name, url, authToken
   });
+}
+
+export function getNewRepoPage(req, res) {
+  // Just render the page with empty input fields
+  renderNewRepoPage( req, res );
 }
 
 export function postNewRepo(req, res) {
   
   if( req.csrfError ) {
-    // TODO: Show an error message
-    res.redirect(`/repos`);
-    return;
+    // As we have an csrf error we need to use the unsafeBody object instead
+    const {name, url, authToken}= req.unsafeBody;
+    return renderNewRepoPage( req, res, name, url, authToken, ErrorMessages.CSRF() );
   }
 
-  // TODO: Validation of the body data
+  const {value, error}= newRepositoryValidator.validate( req.body );
+  if( error ) {
+    const {name, url, authToken}= req.body;
+    return renderNewRepoPage( req, res, name, url, authToken, ErrorMessages.Invalid('repository', error.message) );
+  }
 
-  const {name, url, authToken, type}= req.body;
+  const {name, url, authToken, type}= value;
 
-  // Check if repo with this url already exists
   // Create new repository on the api bridge
+  // Get the name of the repo if none is set here
+
   // Create scheduler default schedule
   // Run scheduler task now
 
--- src/frontend/routes/settings.js
+++ src/frontend/routes/settings.js
@@ -1,5 +1,40 @@
+import Joi from 'joi';
 import { createToken } from '../lib/csrf.js';
 import { ensureUser, setUserRepoSettings, database, getUserRepoSettings } from '../lib/database.js';
+import { ErrorMessages } from '../lib/error-messages.js';
+
+const settingsValidator= Joi.object({
+  isFavorite: Joi.string().valid('on').default('').label('Favorite Flag'), // Checkboxes only set an 'on' value when they are checked
+  isActive: Joi.string().valid('on').default('').label('Active Flag'),
+  repoColor: Joi.string().pattern(/^#[A-Fa-f0-9]{6}$/).required().label('Repository Color'),
+  repoName: Joi.string().trim().max(500).required().label('Name'),
+  repoUrl: Joi.string().uri().max(255).required().label('URL'),
+  repoAuthToken: Joi.string().trim().max(100).required().label('Authentication Token'),
+  repoType: Joi.string().valid('gitlab', 'github').required().label('Repository Type')
+}).unknown(true).required(); // Allow unknown fields for other stuff like csrf tokens
+
+function renderSettingsPage( req, res, repo, errorMessage= null ) {
+  res.render('settings', {
+    user: req.user,
+    repo,
+    errorMessage,
+    csrfToken: createToken( req.sessionID )
+  });
+}
+
+function repoDataFromFormBody( uuid, body ) {
+  return {
+    uuid,
+    isFavorite: !!(body.isFavorite || ''),
+    color: body.repoColor || '#bababa',
+    name: body.repoName || '',
+    isActive: !!(body.isActive || ''),
+    url: body.repoUrl || '',
+    authToken: body.repoAuthToken || '',
+    type: body.repoType || 'gitlab'
+  };
+}
+
 
 export function getSettingsPage(req, res) {
   const repoUuid= req.params.repoUuid;
@@ -11,11 +46,14 @@ export function getSettingsPage(req, res) {
   // TODO: Get the repo settings from the api bridge service
   // TODO: Get the repo settings from the scheduler
 
+  // FIXME: We only show a name if we currently have user settings stored for the repo 
+  // -> Just always take the name from the api_bridge instead
+
   const repo= {
     uuid: repoUuid,
     isFavorite: userSettings.is_favorite || false,
-    color: userSettings.color || 'bababa',
-    name: `repo-${repoUuid}`,
+    color: '#'+ (userSettings.color || 'bababa'),
+    name: userSettings.name,
     isActive: true,
     url: 'https://www.gitlab.com',
     authToken: 'abcdefg',
@@ -24,11 +62,7 @@ export function getSettingsPage(req, res) {
     get isGitLab() { return this.type === 'gitlab' }
   };
 
-  res.render('settings', {
-    user: req.user,
-    repo,
-    csrfToken: createToken( req.sessionID )
-  });
+  renderSettingsPage( req, res, repo );
 }
 
 export function postSettings(req, res) {
@@ -36,15 +70,19 @@ export function postSettings(req, res) {
   const userUuid= req.user.sub;
   
   if( req.csrfError ) {
-    // TODO: Show an error message
-    res.redirect(`/dashboard/${repoUuid}/settings`);
-    return;
+    // As we have an csrf error we need to use the unsafeBody object instead
+    return renderSettingsPage( req, res, repoDataFromFormBody(repoUuid, req.unsafeBody), ErrorMessages.CSRF() );
   }
 
-  // TODO: Validation of the body data
+  // Validate form data
+  const {error, value}= settingsValidator.validate( req.body );
+  if( error ) {
+    return renderSettingsPage( req, res, repoDataFromFormBody(repoUuid, req.body), ErrorMessages.Invalid('settings', error.message) );
+  }
 
-  const {isFavorite, isActive, repoColor, repoName, repoUrl, repoAuthToken, repoType}= req.body;
-  console.log('Got settings:', req.body);
+  console.log('Got settings:', value);
+  const {isFavoriteString, isActiveString, repoColor, repoName, repoUrl, repoAuthToken, repoType}= value;
+  const isFavorite= !!isActiveString, isActive= !!isActiveString;
 
   // TODO: Database transaction here so we rollback if we fail here somewhere
   ensureUser( userUuid );
