2f727802783b0a31d546c7ade9af030e003aee41
Christoph Neubauer <e12023172@student.tuwien.ac.at>
2024-11-15 11:26:24 +0100

    Refactor: Rename ensureUser to addNewUser to clarify what function does

--- src/frontend/routes/repos.js
+++ src/frontend/routes/repos.js
@@ -1,13 +1,11 @@
-import { getUserRepoList, ensureUser } from '../lib/database.js';
+import { getUserRepoList, addNewUser } from '../lib/database.js';
 
 
 export function repos(req, res) {
   const userUuid= req.user.sub;
 
-  ensureUser( userUuid );
+  addNewUser( userUuid );
   const repos= getUserRepoList( userUuid );
-
-  console.log('Repos', repos);
 
   const favoriteRepos= repos.filter( repo => repo.is_favorite );
   const activeRepos= repos.filter( repo => repo.is_active && !repo.is_favorite );
