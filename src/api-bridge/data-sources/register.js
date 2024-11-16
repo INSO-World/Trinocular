d02b9f237be071e1bff76a8a47219cf7fba70273
Oliver Mayer <e12023147@student.tuwien.ac.at>
2024-11-16 12:44:01 +0100

    API-Bridge: Implement issues endpoint
    Ref: #27

--- src/api-bridge/data-sources/register.js
+++ src/api-bridge/data-sources/register.js
@@ -1,8 +1,10 @@
 import { Members } from './members.js';
 import {Details} from "./details.js";
+import {Issues} from './issues.js';
 
 
 export function registerDataSources( registerFunction ) {
   registerFunction( new Members() );
   registerFunction( new Details() );
+  registerFunction( new Issues() );
 }
