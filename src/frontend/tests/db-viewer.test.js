a9f79d62e100a8506fc3e46be3ec8803ec3ae054
Christoph Neubauer <e12023172@student.tuwien.ac.at>
2024-11-18 13:12:52 +0100

    Frontend: Write unit tests for frontend service
    #47

--- src/frontend/tests/db-viewer.test.js
+++ src/frontend/tests/db-viewer.test.js
@@ -1,22 +1,26 @@
 import sinon from 'sinon';
-import { expect } from 'chai';
+import esmock from 'esmock';
+import {expect} from 'chai';
 import {dbViewer} from '../routes/db-viewer.js';
 import {dumpAllTables} from "../lib/database.js";
 
 describe('DB-Viewer Route', () => {
-    let req, res, envStub, dbStub;
+    let req, res, envStub, dumpAllTablesStub;
 
-    beforeEach(() => {
+    beforeEach(async () => {
         req = {
-            method: 'GET',
-            path: '/'
+            method: 'GET'
         };
         res = {
             sendStatus: sinon.stub(),
+            status: sinon.stub()
         }
         envStub = sinon.stub(process, 'env').get(() => ({
             ...process.env
         }))
+
+        dumpAllTablesStub = sinon.stub().resolves([]);
+
     })
 
     afterEach(() => {
@@ -25,15 +29,29 @@ describe('DB-Viewer Route', () => {
 
     describe('db-viewer', () => {
         it('should send status 404 when environment flag is not set', async () => {
-            envStub.value({...process.env, ENABLE_DB_VIEWER: false});
-            await dbViewer(req,res);
+            envStub.value({ENABLE_DB_VIEWER: 'false'});
+            await dbViewer(req, res);
+
             expect(res.sendStatus.calledWith(404)).to.be.true;
         });
 
-        it('should send status 200 when environment flag is set',  async () => {
-            envStub.value({...process.env, ENABLE_DB_VIEWER: true});
-            await dbViewer(req,res);
-            expect(res.sendStatus.calledWith(200)).to.be.true;
+        //TODO try mocking called functions another time
+        /*
+        it('should send status 200 when environment flag is set', async () => {
+            envStub.value({ENABLE_DB_VIEWER: 'true'});
+
+            const {dbViewerStub} = await esmock('../routes/db-viewer.js',
+                {
+                    '../routes/db-viewer.js': {
+                        dumpAllTables: dumpAllTablesStub,
+                        dbViewer: (req,res) => dbViewer(req,res)
+                    }
+                });
+            //dumpAllTablesStub.returns([]);
+            await dbViewerStub.dbViewer(req, res);
+            expect(res.status.calledWith(200)).to.be.true;
         });
-    })
+         */
+    });
 })
+;
