785cad7a75c5c787156051e729a11391a4388bef
Christoph Neubauer <e12023172@student.tuwien.ac.at>
2024-11-18 08:48:07 +0100

    Frontend: Show the correct repository name in dashboard
    #41

--- src/frontend/routes/dashboard.js
+++ src/frontend/routes/dashboard.js
@@ -1,5 +1,6 @@
 import { repositoryIsCurrentlyImporting } from '../lib/currently-importing.js';
 import { visualizations } from '../lib/visualizations.js';
+import {getRepositoryNameByUuid} from '../lib/database.js';
 
 export function dashboard(req, res) {
   // Redirect to the waiting page in case we are currently importing the
@@ -9,8 +10,14 @@ export function dashboard(req, res) {
     return res.redirect(`/wait/${repoUuid}`);
   }
 
-  // TODO: Do a db lookup for the repo name
-  const repoName= `repo-${repoUuid}`;
+  // default to uuid when no name is found in database
+  let repoName= `repo-${repoUuid}`;
+  try {
+    repoName= getRepositoryNameByUuid(repoUuid);
+  } catch(e) {
+    //TODO redirect to repo list or show error "repo with uuid not found"
+    console.error(`Repository with uuid: ${repoUuid} not found: ${e.message}`);
+  }
 
   const visArray= [...visualizations.values()];
   const defaultVisualization= visArray[0];
