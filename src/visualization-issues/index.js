fb56bf9a16fac18e9794025b4a631c84a6d747af
Oliver Mayer <e12023147@student.tuwien.ac.at>
2024-11-28 22:55:33 +0100

    Vis: Init issue visualization service
    Ref: #75

---
+++ src/visualization-issues/index.js
@@ -0,0 +1,41 @@
+
+import http from 'node:http';
+import express from 'express';
+import { passport, protectedOrInternal, sessionAuthentication } from '../auth-utils/index.js';
+import { readSecretEnv, registerService, setupShutdownSignals } from '../common/index.js';
+import { routes } from './routes/routes.js';
+
+
+// TODO: Register visualizations of this service here
+await registerService( process.env.VISUALIZATION_GROUP_NAME, process.env.SERVICE_NAME, {
+  visualizations: [
+    {
+      name: `${process.env.SERVICE_NAME}-burndown-chart`,
+      displayName: 'Issues - Burndown chart',
+      framePath: 'index.html?show=per-issue'
+    },
+  ]
+});
+
+const app = express();
+const server= http.createServer(app);
+
+app.set('unauthenticated redirect', '/');
+
+// Install middleware
+app.use( sessionAuthentication() );
+app.use( protectedOrInternal );
+app.use( express.static('./public') );
+
+// Default user data serialization/deserialization
+passport.serializeUser( (user, done) => done(null, user) );
+passport.deserializeUser( (user, done) => done(null, user) );
+
+
+app.use( routes );
+
+server.listen(80, () => {
+  console.log(`Visualization (issues) service listening at port 80`);
+});
+
+setupShutdownSignals(server);
