1fc96ae198777599731cb8af01e5232ac6dd7a34
Oliver Mayer <e12023147@student.tuwien.ac.at>
2024-11-29 11:56:04 +0100

    Vis: Issue visualization, Removed local testing lines
    Ref: #75

--- src/visualization-issues/routes/routes.js
+++ src/visualization-issues/routes/routes.js
@@ -4,9 +4,7 @@ import {allIssues, rawIssues} from "./all-issues.js";
 
 export const routes= new Router();
 
-//TODO: Change before deployment
-const baseURL= `http://localhost:8080/`;
-// const baseURL= `http://${process.env.HOST_BASE_NAME}/${process.env.SERVICE_NAME}`;
+const baseURL= `http://${process.env.HOST_BASE_NAME}/${process.env.SERVICE_NAME}`;
 const indexPage= templateFile( import.meta.dirname+ '/../views/index.template.html', {baseURL} );
 
 routes.get(['/', '/index.html'], (req, res) => res.type('html').send(indexPage) );
