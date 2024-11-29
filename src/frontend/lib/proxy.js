40baaf422e313378cbd2821b3b280b7caa54b235
Michael Trauner <e12019868@student.tuwien.ac.at>
2024-11-29 16:23:00 +0100

    Frontend: Fixed proxy cutting off query params
    Issue #75

--- src/frontend/lib/proxy.js
+++ src/frontend/lib/proxy.js
@@ -13,9 +13,9 @@ export function visualizationProxy(proxyServer) {
     }
 
     // Extract the name of the targeted visualization
-    const start = req.path.indexOf(basePath) + basePath.length;
-    const end = req.path.indexOf('/', start);
-    const hostname = req.path.substring(start, end > start ? end : req.path.length);
+    const start = req.url.indexOf(basePath) + basePath.length;
+    const end = req.url.indexOf('/', start);
+    const hostname = req.url.substring(start, end > start ? end : req.url.length);
 
     // Bail with 404 status if the hostname does not exist
     if (!visualizationHostnames.has(hostname)) {
@@ -25,9 +25,9 @@ export function visualizationProxy(proxyServer) {
 
     // Re-target the URL before proxying to the visualization hostname
     const target = 'http://' + hostname;
-    req.url = end > start ? req.path.substring(end) : '/';
+    req.url = end > start ? req.url.substring(end) : '/';
 
-    console.log('Proxying to vis:', target + req.path);
+    console.log('Proxying to vis:', target + req.url);
 
     proxyServer.web(req, res, { target }, error => console.error('Proxy error:', error));
   };
