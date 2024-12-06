9c5b00b7035fcdd2e9c7f3c8ef7f99803eca845f
Oliver Mayer <e12023147@student.tuwien.ac.at>
2024-12-06 18:03:21 +0100

    Test: Add first Tests for common
    Ref: #100

---
+++ src/common/tests/env.test.js
@@ -0,0 +1,73 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import esmock from 'esmock';
+
+describe('readSecretEnv', () => {
+  let nodeFsModuleStub;
+
+  async function mockNodeFsImport() {
+    return await esmock('../env.js', {
+      'node:fs': nodeFsModuleStub,
+    });
+  }
+
+  beforeEach(() => {
+    nodeFsModuleStub= {
+      readFileSync: sinon.stub().returns('secret file contents\n')
+    };
+  });
+
+  it('ignores regular variables', async () => {
+    const { readSecretEnv }= await mockNodeFsImport();
+
+    const initialEnv= {
+      REGULAR_VAR: '10',
+      A_SECRET_VAR: 'secret'
+    };
+
+    process.env= {...initialEnv};
+
+    readSecretEnv();
+
+    expect(process.env).to.deep.equal( initialEnv );
+    expect(nodeFsModuleStub.readFileSync.called).to.be.false;
+  });
+
+  it('loads secret variables from file', async () => {
+    const { readSecretEnv }= await mockNodeFsImport();
+
+    const initialEnv= {
+      REGULAR_VAR: '10',
+      A_SECRET_VAR: 'secret',
+      A_SECRET_FILE: '/run/secrets/a/path/file.txt'
+    };
+
+    process.env= {...initialEnv};
+
+    readSecretEnv();
+
+    expect(process.env).to.deep.equal({
+      ...initialEnv,
+      A_SECRET: 'secret file contents'
+    });
+    expect(nodeFsModuleStub.readFileSync.calledOnce).to.be.true;
+    expect(nodeFsModuleStub.readFileSync.calledWith('/run/secrets/a/path/file.txt', 'utf-8')).to.be.true;
+  });
+
+  it('ignores secret variables with bad path', async () => {
+    const { readSecretEnv }= await mockNodeFsImport();
+
+    const initialEnv= {
+      REGULAR_VAR: '10',
+      A_SECRET_VAR: 'secret',
+      A_SECRET_FILE: '/wrong/path/file.txt'
+    };
+
+    process.env= {...initialEnv};
+
+    readSecretEnv();
+
+    expect(process.env).to.deep.equal( initialEnv );
+    expect(nodeFsModuleStub.readFileSync.called).to.be.false;
+  });
+});
---
+++ src/common/tests/template.test.js
@@ -0,0 +1,90 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import esmock from 'esmock';
+import { templateString } from '../template.js';
+
+describe('String Templating', () => {
+  let nodeFsModuleStub;
+
+  async function mockNodeFsImport() {
+    return await esmock('../template.js', {
+      'node:fs': nodeFsModuleStub,
+    });
+  }
+
+  beforeEach(() => {
+    nodeFsModuleStub= {
+      readFileSync: sinon.stub().returns('')
+    };
+  });
+
+  describe('templateString', () => {
+    it('leaves plain strings as is', () => {
+      const input= 'A plain old string';
+      const output= templateString( input );
+      expect(output).to.equal(input);
+    });
+
+    it('ignores empty strings', () => {
+      const input= '';
+      const output= templateString( input );
+      expect(output).to.equal(input);
+    });
+
+    it('ignores single and triple curlies', () => {
+      const input= 'a string with {some text} and {{{and more text}}}';
+      const output= templateString( input );
+      expect(output).to.equal(input);
+    });
+
+    it('inserts values from array by index', () => {
+      const input= 'a string with {{1}}, {{0}}, {{ 2 }}, {{  3  }}';
+      const output= templateString( input, ['abc', 'def', 10, {a: 1}] );
+      expect(output).to.equal('a string with def, abc, 10, [object Object]');
+    });
+
+    it('inserts values from object by key', () => {
+      const input= 'a string with {{a}}, {{b}}, {{ c }}, {{  d  }}';
+      const output= templateString( input, {b: 'abc', a: 'def', c: 10, d: {a: 1}} );
+      expect(output).to.equal('a string with def, abc, 10, [object Object]');
+    });
+
+    it('inserts values from map by key', () => {
+      const map= new Map();
+      map.set('b', 'abc');
+      map.set('a', 'def');
+      map.set('c', 10);
+      map.set('d', {a: 1});
+
+      const input= 'a string with {{a}}, {{b}}, {{ c }}, {{  d  }}';
+      const output= templateString( input, map );
+      expect(output).to.equal('a string with def, abc, 10, [object Object]');
+    });
+  });
+
+  describe('templateFile', () => {
+    it('ignores empty file', async () => {
+      const { templateFile }= await mockNodeFsImport();
+
+      nodeFsModuleStub.readFileSync.returns('');
+
+      const output= templateFile('/a/path/file.txt');
+
+      expect(output).to.equal('');
+      expect(nodeFsModuleStub.readFileSync.calledOnce).to.be.true;
+      expect(nodeFsModuleStub.readFileSync.calledWith('/a/path/file.txt', 'utf-8')).to.be.true;
+    });
+
+    it('formats file', async () => {
+      const { templateFile }= await mockNodeFsImport();
+
+      nodeFsModuleStub.readFileSync.returns('a string with {{1}}, {{0}}');
+
+      const output= templateFile('/a/path/file.txt', ['abc', 'def']);
+
+      expect(output).to.equal('a string with def, abc');
+      expect(nodeFsModuleStub.readFileSync.calledOnce).to.be.true;
+      expect(nodeFsModuleStub.readFileSync.calledWith('/a/path/file.txt', 'utf-8')).to.be.true;
+    });
+  });
+});
