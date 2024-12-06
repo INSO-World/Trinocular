6936340463de2622af0388c0ee698de7df2e0b46
Oliver Mayer <e12023147@student.tuwien.ac.at>
2024-12-06 18:04:53 +0100

    Test: Fixed bug discovered by env.test.js
    Ref: #100

--- src/common/env.js
+++ src/common/env.js
@@ -5,13 +5,13 @@ export function readSecretEnv() {
     const varValue = process.env[varName];
     if (varName.toLowerCase().endsWith('secret_file')) {
       // Only actually load files with docker compose secrets
+      const secretName = varName.substring(0, varName.length - 5); // Takeoff the '_FILE' postfix from the name
       if (!varValue.startsWith('/run/secrets')) {
         console.log(`Ignoring secret variable '${secretName}' with unexpected path`);
         continue;
       }
 
       const fileContents = fs.readFileSync(varValue, 'utf-8').trim();
-      const secretName = varName.substring(0, varName.length - 5); // Takeoff the '_FILE' postfix from the name
       process.env[secretName] = fileContents;
 
       console.log(`Loaded secret '${secretName}' into environment`);
