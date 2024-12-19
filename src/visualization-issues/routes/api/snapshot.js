e5f9fe393692c5e9237d5767ac3c1c041aad33a0
Oliver Mayer <e12023147@student.tuwien.ac.at>
2024-12-19 17:04:47 +0100

    Vis: Visualization issues, refactored snapshot
    Ref: #119

--- src/visualization-issues/routes/api/snapshot.js
+++ src/visualization-issues/routes/api/snapshot.js
@@ -3,8 +3,8 @@ import {
   getRepositoryForUuid
 } from '../../lib/requests.js';
 import { getDynamicDateRange, mapDataToRange } from '../../lib/burndown-chart-utils.js';
-import { formatInsertManyValues, pool } from '../../../postgres-utils/index.js';
 import { sendSchedulerCallback } from '../../../common/index.js';
+import { insertBurndownChartData } from '../../lib/database.js';
 
 export async function postSnapshot(req, res) {
   const { transactionId } = req.query;
@@ -13,103 +13,38 @@ export async function postSnapshot(req, res) {
   res.sendStatus(200);
 
   // 1. Fetch all repos from api-bridge
-  const tmp = await getRepositoryForUuid(uuid);
-  const repos = tmp.data;
+  const { getRepoError, data } = await getRepositoryForUuid(uuid);
+  const repo = data[0];
+  if (getRepoError) {
+    console.error(getRepoError);
+    return null;
+  }
 
-  // 2. Per Repo fetch issues from api-bridge
-  const issuePromises = repos.map(async repo => {
-    const { error, data: issueData } = await getDatasourceForRepositoryFromApiBridge(
-      'issues',
-      uuid
-    );
+  // 2. Fetch issues from api-bridge
+  const { getDataSourceError, data: issueData } = await getDatasourceForRepositoryFromApiBridge(
+    'issues',
+    uuid
+  );
 
-    if (error) {
-      console.error(error);
-      return null;
-    }
-    // 3. Process issues to get burndown data
-    const dataRange = getDynamicDateRange(issueData, repo);
-    const filledData = mapDataToRange(issueData, dataRange);
+  if (getDataSourceError) {
+    console.error(getDataSourceError);
+    return null;
+  }
 
-    return { burndownIssues: filledData, uuid: repo.uuid };
-  });
-  const reposIssues = await Promise.all(issuePromises);
+  // 3. Process issues to get burndown data
+  const dataRange = getDynamicDateRange(issueData, repo);
+  const filledData = mapDataToRange(issueData, dataRange);
 
   // 4. Store burndown data in database
-  const dbPromisesDay = reposIssues.map(async repoIssues => {
-    const { valuesString, parameters } = formatInsertManyValues(
-      repoIssues.burndownIssues.dailyData,
-      (parameters, issue) => {
-        parameters.push(uuid, issue.date, issue.openIssues, issue.open_issues_info);
-      }
-    );
+  const insertPromises = [
+    insertBurndownChartData(uuid, filledData.dailyData, 'day'),
+    insertBurndownChartData(uuid, filledData.weeklyData, 'week'),
+    insertBurndownChartData(uuid, filledData.monthlyData, 'month')];
 
-    const result = await pool.query(
-      `INSERT INTO issue_day (uuid, date, open_issues, open_issues_info)
-      VALUES
-      ${valuesString}
-   ON CONFLICT ON CONSTRAINT unique_uuid_day
-      DO UPDATE SET
-      id = EXCLUDED.id,
-      date = EXCLUDED.date,
-      open_issues = EXCLUDED.open_issues,
-      open_issues_info = EXCLUDED.open_issues_info
-      RETURNING id`,
-      parameters
-    );
-  });
-  await Promise.all(dbPromisesDay);
-  // 4. Store burndown data in database
-  const dbPromisesWeek = reposIssues.map(async repoIssues => {
-    const { valuesString, parameters } = formatInsertManyValues(
-      repoIssues.burndownIssues.weeklyData,
-      (parameters, issue) => {
-        parameters.push(uuid, issue.date, issue.openIssues, issue.open_issues_info);
-      }
-    );
-
-    const result = await pool.query(
-      `INSERT INTO issue_week (uuid, date, open_issues, open_issues_info)
-      VALUES
-      ${valuesString}
-   ON CONFLICT ON CONSTRAINT unique_uuid_week
-      DO UPDATE SET
-      id = EXCLUDED.id,
-      date = EXCLUDED.date,
-      open_issues = EXCLUDED.open_issues,
-      open_issues_info = EXCLUDED.open_issues_info
-      RETURNING id`,
-      parameters
-    );
-  });
-  await Promise.all(dbPromisesWeek);
-  // 4. Store burndown data in database
-  const dbPromisesMonth = reposIssues.map(async repoIssues => {
-    const { valuesString, parameters } = formatInsertManyValues(
-      repoIssues.burndownIssues.monthlyData,
-      (parameters, issue) => {
-        parameters.push(uuid, issue.date, issue.openIssues, issue.open_issues_info);
-      }
-    );
-
-    const result = await pool.query(
-      `INSERT INTO issue_month (uuid, date, open_issues, open_issues_info)
-      VALUES
-      ${valuesString}
-   ON CONFLICT ON CONSTRAINT unique_uuid_month
-      DO UPDATE SET
-      id = EXCLUDED.id,
-      date = EXCLUDED.date,
-      open_issues = EXCLUDED.open_issues,
-      open_issues_info = EXCLUDED.open_issues_info
-      RETURNING id`,
-      parameters
-    );
-  });
-  await Promise.all(dbPromisesMonth);
+  await Promise.all(insertPromises);
 
   // 5. Send callback
-  console.log(`Visualization '${process.env.SERVICE_NAME}' creates snapshot...`);
+  console.log(`Visualization '${process.env.SERVICE_NAME}' creates snapshot for uuid: ${uuid}`);
 
   await sendSchedulerCallback(transactionId, 'ok');
 }
