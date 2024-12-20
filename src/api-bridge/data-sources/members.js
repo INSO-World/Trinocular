0e4419e7fa6e285c83308b8c4469c66e35199245
Christoph Neubauer <e12023172@student.tuwien.ac.at>
2024-12-20 14:28:18 +0100

    API: Improve filtering bot users
    #120

--- src/api-bridge/data-sources/members.js
+++ src/api-bridge/data-sources/members.js
@@ -20,28 +20,16 @@ export class Members extends DataSource {
     const api = repo.api();
     let { data: members } = await api.fetchAll('/projects/:id/members');
 
-    try {
-      // isBot is needed to keep members where only a personal access token is used and no project token
-      const { id: botId, userName: botUsername, isBot } = await api.getAuthTokenAssociatedUser();
+    // filter members that match the bot name pattern
+    const botRegex = /project_[a-fA-F\d]+_bot_[a-fA-F\d]+/;
 
-      // keep members that do not fit id & username of the bot or if the authToken generally is no bot
-      if (isBot) {
-        members = members.filter(({ id, username }) => id !== botId && username !== botUsername);
-      }
-      // TODO: maybe adapt to filter out combination of "PROJECT_ID_bot_"
-      // additionally filter other members that have the typical '_bot_' in their name
-      members = members.filter( ({ username }) => !username.includes('_bot_'));
-
-    } catch (e) {
-      // filter members as long as fetch was successful, otherwise just ignore
-      console.error(`Could not filter authToken bot user from members list:`, e);
-    }
-
-    const records = members.map(({ id, username, name, email }) => ({
-      id,
-      username,
-      name,
-      email
+    const records = members
+      .filter( ({ username }) => !botRegex.test(username))
+      .map(({ id, username, name, email }) => ({
+        id,
+        username,
+        name,
+        email
     }));
 
     const storage = new Storage('members');
