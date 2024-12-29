07b460d11a61c3154fd4c7b406796ca936476cfb
Oliver Mayer <e12023147@student.tuwien.ac.at>
2024-12-29 14:10:58 +0100

    Test: Fixed failing test by mocking the modules instead of local stubbing
    Ref: #104

--- src/api-bridge/tests/index-test.js
+++ src/api-bridge/tests/index-test.js
@@ -2,37 +2,45 @@ import { expect } from 'chai';
 import sinon from 'sinon';
 import request from 'supertest';
 import express from 'express';
-import { routes } from '../routes/routes.js';
-import { ApiBridge } from "../lib/api-bridge.js";
+import esmock from 'esmock';
 
+process.env.INTERNAL_API_SECRET = 'internalAPItoken';
 describe('Routes', () => {
   let app, serverStub;
 
-  // process.env.INTERNAL_API_SECRET = "internalAPItoken";
   const commonHeaders = {
     Authorization: `bearer ${process.env.INTERNAL_API_SECRET}`
-    // Authorization: `bearer internalAPItoken`
   };
 
-  beforeEach(() => {
-    app = express();
-    app.use(express.json());
-    app.use(routes);
-
+  beforeEach(async () => {
     serverStub = {
       getAllRepositories: sinon.stub(),
       getRepository: sinon.stub(),
       postRepository: sinon.stub(),
       deleteRepository: sinon.stub(),
       putRepository: sinon.stub(),
-      postSnapshot: sinon.stub(),
+      postSnapshot: sinon.stub()
     };
 
-    sinon.stub(ApiBridge, 'the').returns({
-      registerDataSource: sinon.stub().returns(serverStub),
-      init: sinon.stub().returns(serverStub),
-      routes: sinon.stub().returns(serverStub)
+    let dummyRequestHandler = sinon.stub().callsFake((req, res) => {
+      return res.json({ data: { 'test': 'testy' } });
     });
+    const routesMock = await esmock('../routes/routes.js', {
+      '../routes/repository.js': {
+        deleteRepository: dummyRequestHandler,
+        getAllRepositories: dummyRequestHandler,
+        getRepository: dummyRequestHandler,
+        postRepository: dummyRequestHandler,
+        putRepository: dummyRequestHandler
+      },
+      '../routes/snapshot.js': {
+        postSnapshot: dummyRequestHandler
+      }
+    });
+
+    app = express();
+    app.use(express.json());
+    app.use(routesMock.routes);
   });
 
   afterEach(() => {
@@ -50,12 +58,20 @@ describe('Routes', () => {
 
   describe('GET /repository', () => {
     it('should exist', async () => {
-      const repoData = {data: 'repos'};
-
-      serverStub.getAllRepositories.returns(repoData);
-
       await request(app)
         .get('/repository')
+        .set(commonHeaders)
+        .expect(200);
+    });
+  });
+
+  describe('POST /repository', () => {
+    it('should exist', async () => {
+      const repoData = { data: 'repo' };
+
+      await request(app)
+        .post(`/repository`)
+        .send(repoData)
         .set(commonHeaders)
         .expect(200);
     });
@@ -64,9 +80,6 @@ describe('Routes', () => {
   describe('GET /repository/:uuid', () => {
     it('should exist', async () => {
       const uuid = 'repo-uuid';
-      const repoData = {data: 'repo'};
-
-      serverStub.getRepository.returns(repoData);
 
       await request(app)
         .get(`/repository/${uuid}`)
@@ -78,8 +91,7 @@ describe('Routes', () => {
   describe('PUT /repository/:uuid', () => {
     it('should exist', async () => {
       const uuid = 'repo-uuid';
-      const repoData = {data: 'updated-repo'};
-      serverStub.putRepository.returns(true);
+      const updatedRepo = { data: 'updated-repo' };
 
       await request(app)
         .put(`/repository/${uuid}`)
@@ -102,31 +114,15 @@ describe('Routes', () => {
     });
   });
 
-  describe('POST /repository/:uuid', () => {
-    it('should exist', async () => {
-      const uuid = 'repo-uuid';
-      const repoData = {data: 'repo'};
-
-      serverStub.getRepository.returns(repoData);
-
-      await request(app)
-        .post(`/repository/${uuid}`)
-        .set(commonHeaders)
-        .expect(200);
-    });
-  });
-
   describe('POST /snapshot/:uuid', () => {
     it('should exist', async () => {
       const uuid = 'repo-uuid';
-
-      serverStub.postSnapshot.returns(true);
 
       await request(app)
         .post(`/snapshot/${uuid}`)
         .set(commonHeaders)
         .send({ message: 'Create snapshot' })
-        .expect(201);
+        .expect(200);
     });
   });
 });
