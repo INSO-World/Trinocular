5fc4de68279b5d0c7f1825c328906d74e6e726a1
Oliver Mayer <e12023147@student.tuwien.ac.at>
2025-01-13 15:56:14 +0100

    Vis: Pipelines, Change map like object to list
    Easier code because forEach can be used later
    
    Ref: #130

--- src/visualization-pipelines/routes/pipeline-runs.js
+++ src/visualization-pipelines/routes/pipeline-runs.js
@@ -4,8 +4,8 @@ export async function loadPipelineRunsFromDatabase(req, res) {
   const pipelineData = await getPipelineRunsData(req.query.repo);
 
   const groupedData = pipelineData.reduce((acc, { branch, date, success_count, failed_count }) => {
-    if (!acc[branch]) acc[branch] = {};
-    acc[branch][date] = { success_count, failed_count };
+    if (!acc[branch]) acc[branch] = [];
+    acc[branch].push({ date, success_count, failed_count });
     return acc;
   }, {});
 
