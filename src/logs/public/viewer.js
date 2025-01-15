2c3748cd82c68b24090160c36be171e8b21078bb
Matthias Preymann <matthias.preymann@student.tuwien.ac.at>
2025-01-15 20:41:23 +0100

    Logs: Update the viewer page in-place
     This way we can submit the viewer filter form without creating history entry,

--- src/logs/public/viewer.js
+++ src/logs/public/viewer.js
@@ -1,15 +1,48 @@
 
-const filterForm= document.getElementById('filter-form');
-filterForm.onsubmit= e => {
-  console.log('submit');
-};
+function setupEventHandlers() {
+  const filterForm= document.getElementById('filter-form');
+  filterForm.onsubmit= async e => {
+    e.preventDefault();
 
-document.querySelectorAll('[data-auto-submit]').forEach( element => {
-  element.onchange= () => {
-    if( element.name !== 'page' ) {
-      filterForm.elements.namedItem('page').value= 0;
+    const form = e.target;
+    const formData = new FormData(form);
+    const url = new URL(form.action);
+    url.search = new URLSearchParams(formData).toString();
+
+    try {
+      const response = await fetch(url, {
+        method: 'GET',
+        redirect: 'error'
+      });
+
+      const data = await response.text();
+
+      if( !response.ok ) {
+        throw new Error(`Server did not respond ok: ${data}`);
+      }
+
+      // Replace the entire page content with the response data
+      document.open();
+      document.write(data);
+      document.close();
+      setupEventHandlers();
+
+      // Update the URL without adding a history entry
+      window.history.replaceState({}, '', url);
+    } catch (error) {
+      console.error('Error:', error);
     }
-
-    filterForm.submit();
   };
-});
+
+  document.querySelectorAll('[data-auto-submit]').forEach( element => {
+    element.onchange= () => {
+      if( element.name !== 'page' ) {
+        filterForm.elements.namedItem('page').value= 0;
+      }
+  
+      filterForm.requestSubmit();
+    };
+  });
+}
+
+setupEventHandlers();
