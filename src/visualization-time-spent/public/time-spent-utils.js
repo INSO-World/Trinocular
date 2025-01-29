01f84c66ae3af7b628ad88993f2750c0bdf9d62c
Matthias Preymann <matthias.preymann@student.tuwien.ac.at>
2025-01-29 23:28:27 +0100

    Vis: Time-Spent: Stretch out the data set with dummy data points
    The same as commit 3070211 but even more scuffed.
    
    #153

--- src/visualization-time-spent/public/time-spent-utils.js
+++ src/visualization-time-spent/public/time-spent-utils.js
@@ -28,19 +28,80 @@ export function filterIssuesByCreationDate(data, startDate, endDate) {
 }
 
 /**
- *
- * @param data
- * @param {Date} startDate
- * @param {Date} endDate
- * @returns {*}
+ * @typedef {{spent_week: string, cumulative_spent: number, user_id: number, username: string, name: string, email: string? }} DataItem
+ * @param {DataItem[]} data
+ * @param {Date|string} startDate
+ * @param {Date|string} endDate
+ * @returns {DataItem[]}
  */
 export function filterTimelogsBySpentDate(data, startDate, endDate) {
-  const start = startDate;
-  const end = endDate;
+  const start = new Date(startDate);
+  const end = new Date(endDate);
 
-  return data.filter(timelog => {
-    const created = new Date(timelog.spent_week);
+  /**
+   * Helper function that updates a min-max-entry with a data item, so that the entry
+   * stores the min and max points for all items. If no entry is provided a new one is created.
+   * @typedef {{newestDate: Date, newestItem: any, oldestDate: Date, oldestItem: any}} MinMaxEntry
+   * @param {MinMaxEntry?} minmax 
+   * @param {DataItem} item 
+   * @returns {MinMaxEntry}
+   */
+  function updateMinMax( minmax, item ) {
+    const created= new Date(item.spent_week);
+
+    if( !minmax ) {
+      return {
+        newestDate: created,
+        newestItem: item,
+        oldestDate: created,
+        oldestItem: item
+      };
+    }
+
+    if( minmax.newestDate < created ) {
+      minmax.newestDate= created;
+      minmax.newestItem= item;
+    }
+
+    if( minmax.oldestDate > created ) {
+      minmax.oldestDate= created;
+      minmax.oldestItem= item;
+    }
+
+    return minmax;
+  }
+
+  // Filter all items in the selected start-end-time-span
+  const filtered= data.filter(item => {
+    const created = new Date(item.spent_week);
     return created >= start && created <= end;
   });
+
+  // Find the newest and oldest item for each author, and the dates of the newest and oldest
+  // items in the filtered dataset
+  /** @type {Map<number, MinMaxEntry>} */
+  const authors= new Map();
+  let timespanMinMax= null;
+  for( const item of filtered ) {
+    timespanMinMax= updateMinMax( timespanMinMax, item );
+    authors.set(item.user_id, updateMinMax(authors.get(item.user_id), item) );
+  }
+
+  // Add dummy values to stretch the data if the time span is larger than
+  // a week in one of the directions
+  const sixDays= 6 * 24 * 60 * 60 * 1000;
+  if( timespanMinMax && (end - timespanMinMax.newestDate) > sixDays ) {
+    authors.forEach( authorMinMax => {
+      filtered.push({...authorMinMax.newestItem, spent_week: end.toISOString()});
+    });
+  }
+
+  if( timespanMinMax && (timespanMinMax.oldestDate - start) > sixDays ) {
+    authors.forEach( authorMinMax => {
+      filtered.push({...authorMinMax.oldestItem, spent_week: start.toISOString()});
+    });
+  }
+
+  return filtered;
 }
 
