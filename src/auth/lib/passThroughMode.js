18130b91c6d86751930614aae04b7d6cf293023e
Matthias Preymann <matthias.preymann@student.tuwien.ac.at>
2025-11-08 00:41:22 +0100

    Auth: Add bool type to flag getter function

--- src/auth/lib/passThroughMode.js
+++ src/auth/lib/passThroughMode.js
@@ -1,14 +1,14 @@
 
 import { flagIsSet, logger } from '../../common/index.js';
 
-
+/** @type {boolean?} */
 let passThroughModeValue= null;
 export function isPassThroughMode() {
   if( passThroughModeValue === null ) {
     passThroughModeValue= flagIsSet('PASS_THROUGH_MODE');
 
     if( passThroughModeValue ) {
-      logger.warning('Service is running in pass-through mode. User authentication are disabled')
+      logger.warning('Service is running in pass-through mode. User authentication is disabled');
     }
   }
 
