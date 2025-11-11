35f401397f93d368c5957353ecbf82e7bc7283aa
Matthias Preymann <matthias.preymann@student.tuwien.ac.at>
2025-11-11 20:55:19 +0100

    Frontend: Protect admin pages

--- src/frontend/routes/routes.js
+++ src/frontend/routes/routes.js
@@ -4,7 +4,7 @@ import { home } from './home.js';
 import { repos } from './repos.js';
 import { getErrorPage } from './error.js';
 import { notifyRepositoryImported, notifyVisualization } from './api/notify.js';
-import { protectedApi, protectedPage } from '../../auth-utils/index.js';
+import { adminApi, adminPage, protectedApi, protectedPage } from '../../auth-utils/index.js';
 import { internalApi } from '../../common/index.js';
 import { dashboard } from './dashboard.js';
 import { deleteRepository, getSettingsPage, postSettings } from './settings.js';
@@ -20,11 +20,13 @@ export const routes = Router();
 routes.get('/', home);
 routes.get('/error', getErrorPage);
 routes.get('/repos', protectedPage, repos);
-routes.route('/repos/new').get(protectedPage, getNewRepoPage).post(protectedPage, postNewRepo);
+routes.route('/repos/new')
+  .get(protectedPage, adminPage, getNewRepoPage)
+  .post(protectedPage, adminPage, postNewRepo);
 routes
   .route('/dashboard/:repoUuid')
   .get(protectedPage, dashboard)
-  .delete(protectedPage, deleteRepository);
+  .delete(protectedPage, adminApi, deleteRepository);
 routes
   .route('/dashboard/:repoUuid/settings')
   .get(protectedPage, getSettingsPage)
@@ -32,15 +34,15 @@ routes
 routes.get('/wait/:repoUuid', protectedPage, getWaitPage);
 routes.get('/wait/:repoUuid/update', protectedApi, getWaitPageUpdate);
 
-routes.get('/db-viewer', protectedPage, dbViewer);
+routes.get('/db-viewer', protectedPage, adminPage, dbViewer);
 
-routes.get('/status', protectedPage, getStatusPage);
+routes.get('/status', protectedPage, adminPage, getStatusPage);
 
 routes.use('/api/notify', internalApi);
 routes.post('/api/notify/vis', notifyVisualization);
 routes.post('/api/notify/import', notifyRepositoryImported);
 
-routes.post('/api/reimport', protectedApi, reimportRepositories);
+routes.post('/api/reimport', protectedApi, adminApi, reimportRepositories);
 
 routes
   .route('/api/repo/:repoUuid/dashboard-config')
