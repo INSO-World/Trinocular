e45a685d7b085f4a3003927ddf2a40bed285aa64
Matthias Preymann <matthias.preymann@student.tuwien.ac.at>
2025-11-13 16:12:19 +0100

    Frontend: Confirm before reimporting all repos

--- src/frontend/public/status.js
+++ src/frontend/public/status.js
@@ -1,6 +1,10 @@
 
 document.getElementById('reimport-button').addEventListener('click', async event => {
 
+  if(!confirm('Do you really want to delete and re-import all repository data?')) {
+    return;
+  }
+
   // Dumb way to prevent the user from clicking the button immediately again
   event.target.disabled= true;
   
